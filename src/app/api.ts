f626a7dbbabba8b55d2da0cc22a3447b68355e1e
BELLO Mouhoutassim <mouhoutassim.bello@rintio.com>
2021-05-03 11:23:48 +0000

    Update api.ts

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,11 +1,10 @@
 
-
     export class Constant{
 
-    public url_connect = 'http://192.168.9.16:7080/utilisateur/utilisateurs/connexion?mail=';
-    public url_save = 'http://192.168.9.16:7080/utilisateur/utilisateurs?basePath=';
-    public url_accueil= 'http://192.168.9.16:7080/utilisateur/utilisateurs/byMail?mail=';
+    public url_connect = 'http://api.dev.duniyadata.com/utilisateur/utilisateurs/connexion?mail=';
+    public url_save = 'http://api.dev.duniyadata.com/utilisateur/utilisateurs?basePath=';
+    public url_accueil= 'http://api.dev.duniyadata.com/utilisateur/utilisateurs/byMail?mail=';
    // public url_rec = "http://localhost:4200/update";  /// Not used anymore
-    public url_token = 'http://192.168.9.16:7080/utilisateur/utilisateurs/generate_token?mail=';
+    public url_token = 'http://api.dev.duniyadata.com/utilisateur/utilisateurs/generate_token?mail=';
    
 }
